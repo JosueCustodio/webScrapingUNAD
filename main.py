c01741a3273df9e18df7fa3d974b0b4b823ce657
Josué Custodio <78887708+JosueCustodio@users.noreply.github.com>
2025-04-20 00:38:24 -0400

    add option for consult carreras de postgrado

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 import ofertaAcademica
 import consultaCarrera
+import consultaPostgrado
 
 def mostrar_menu():
     print("\n========== MENÚ DE CONSULTA ==========\n")
@@ -9,11 +10,12 @@ def mostrar_menu():
     print("2. Ver Carreras de Postgrado")
     print("3. Ver Facultades")
     print("4. Consultar Informacion de una Carrera de Grado")
-    print("5. Salir del Programa")
+    print("5. Consultar Informacion de una Carrera de Post Grado")
+    print("6. Salir del Programa")
 
 while True:
     mostrar_menu()
-    opcion = input("\n Selecciona una opción (1-5): ")
+    opcion = input("\n Selecciona una opción (1-6): ")
 
     if opcion == "1":
         ofertaAcademica.buscarCarrerasGrado()
@@ -22,8 +24,10 @@ while True:
     elif opcion == "3":
         ofertaAcademica.buscarFacultad()
     elif opcion == "4":
-        consultaCarrera.consultarCarrera()   
+        consultaCarrera.consultarCarrera()
     elif opcion == "5":
+        consultaPostgrado.consultarPostgrado()
+    elif opcion == "6":
         print("Saliendo del programa.")
         break
     else:
